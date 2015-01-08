29374a205411f38d0e44965be31517902eea6f63
David Miller <davidmiller@Jennifers-MacBook-Pro.local>
2015-01-08 15:22:43 -0600

    first commit for storyBoard Built

--- TicTacToe/ViewController.m
+++ TicTacToe/ViewController.m
@@ -10,18 +10,42 @@
 
 @interface ViewController ()
 
+
+@property (strong, nonatomic) IBOutlet UILabel *LabelOne;
+@property (strong, nonatomic) IBOutlet UILabel *LabelTwo;
+@property (strong, nonatomic) IBOutlet UILabel *LabelThree;
+@property (strong, nonatomic) IBOutlet UILabel *LabelFour;
+@property (strong, nonatomic) IBOutlet UILabel *LabelFive;
+@property (strong, nonatomic) IBOutlet UILabel *LabelSix;
+@property (strong, nonatomic) IBOutlet UILabel *LabelSeven;
+@property (strong, nonatomic) IBOutlet UILabel *LabelEight;
+@property (strong, nonatomic) IBOutlet UILabel *LabelNine;
+@property (strong, nonatomic) IBOutlet UILabel *whichPlayerLabel;
+
+@property (strong, nonatomic) IBOutlet UIButton *LabelHelp;
+
+@property (strong, nonatomic) IBOutlet UIButton *LabelGoButton;
+
+@property (strong, nonatomic) IBOutlet UITapGestureRecognizer *myTapGester;
+
+
 @end
 
 @implementation ViewController
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    NSLog(@"viewDidLoad");
+
+}
+-(void)findLabelUsingPoint {
+    NSLog(@"findLabelUsingPoint");
+
 }
 
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+- (IBAction)onLabelTapped:(UITapGestureRecognizer *)sender {
+    NSLog(@"onLabelTapped");
+
 }
 
 @end
